a2657e0010023d0555371afc6ce326028020ff7c
Endrew Sousa <endrew.developer@gmail.com>
2024-10-03 10:21:20 -0300

    Consegui tira o erro, graça ao professor

--- django/filmes/admin.py
+++ django/filmes/admin.py
@@ -4,4 +4,4 @@ from .models import Filmes
 # Register your models here.
 @admin.register(Filmes)
 class FilmeAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name']
+    list_display = ('id', 'nome_filme')
--- django/filmes/views.py
+++ django/filmes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Filmes
+from django.http import JsonResponse
 
 # Create your views here.
+def filmes_view(request):
+    filmes = Filmes.objects.all()
+    data = [{'ID': filme.id, 'Nome': filme.nome_filme, 'Tipo': filme.tipo} for filme in filmes]
+    return JsonResponse(data, safe=False, status=200)
