c4e82a4673b5babda022bf6f8e72a2dff049c89a
Endrew Sousa <endrew.developer@gmail.com>
2024-10-03 01:16:02 -0300

    Mundaça de plano

--- django/api_filme/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import FILME
-# Register your models here.
-
-admin.site.register(FILME)
--- django/api_filme/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import FILME
-
-class FilmeModelForm(forms.ModelForm):
-    class Meta:
-        model = FILME
-        fields = '__all__'
