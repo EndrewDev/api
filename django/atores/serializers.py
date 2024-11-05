8b4d9e16e3a0d06aa9cce5046b821ec7c4a834fc
Endrew Silva <endrew.developer@gmail.com>
2024-11-05 16:32:16 -0300

    Atualização

--- django/atores/serializers.py
+++ django/atores/serializers.py
@@ -6,3 +6,8 @@ class AtorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ator
         fields = '__all__'
+
+    def validate(self, data):
+        if data['ano_lancamento'] < data['data_nascimento']:
+            raise serializers.ValidationError('O ano de lançamento não pode ser anterior ao ano de nascimento do ator.')
+        return data
--- django/filmes/serializers.py
+++ django/filmes/serializers.py
@@ -4,20 +4,20 @@ from django.db.models import Avg
 
 class FilmeSerializer(serializers.ModelSerializer):
 
-    # media_avaliacao = serializers.SerializerMethodField(read_only = True)
+    media_avaliacao = serializers.SerializerMethodField(read_only = True)
 
     class Meta:
         model = Filmes
         fields = '__all__'
 
-    # def get_media_avaliacao(self, instance):
-    #     media_avaliacoes = instance.filmes.aggregate(valor_medio=Avg('estrelas'))['valor_medio']
+    def get_media_avaliacao(self, instance):
+        media_avaliacoes = instance.filmes.aggregate(valor_medio=Avg('estrelas'))['valor_medio']
 
-    #     if media_avaliacoes:
-    #         return round(media_avaliacoes, 1)
-    #     return None
+        if media_avaliacoes:
+            return round(media_avaliacoes, 1)
+        return None
 
-    # def validate_ano_de_lancamento(self, value):
-    #     if value < 2000:
-    #         raise serializers.ValidationError('Não pode cadastrar menor de 2000')
-    #     return value
+    def validated_ano_lancamento(self, value):
+        if value < 2000:    
+            raise serializers.ValidationError('Não pode cadastrar menor de 2000')
+        return value
