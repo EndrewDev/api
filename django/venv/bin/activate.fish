036403b7c3c66bf1bfb6afc0073b309f928e4295
Endrew Sousa <endrew.developer@gmail.com>
2024-12-14 19:46:51 -0300

    update

--- django/venv/bin/activate.fish
+++ django/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/endrew/Developments/api/django/venv
+set -gx VIRTUAL_ENV /home/endrew/developers/api/django/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
